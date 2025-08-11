5f058519b2d2d1fdfd98748cb9cba3c4a6b074f6
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2025-08-11 15:12:19 +0700

    doc (exam): update note for test with delay 12 milisec

--- examples/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_12/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_12.ino
+++ examples/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_12/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_12.ino
@@ -1,8 +1,8 @@
 /* ESP32S3 RECEIVE
 Message is in Standard Format
 
-ID: 1
-Byte: Data: 01 
+ID: 0
+Byte: Data: 00 
 
 Send back the received data!
 
@@ -13,16 +13,8 @@ TX buffered: 0
 
 Message is in Standard Format
 
-ID: 0
-Byte: Data: 00 
-
-Send back the received data!
-
-Message queued for transmission
-Message is in Standard Format
-
-ID: 3
-Byte: Data: 03 
+ID: 1
+Byte: Data: 01 
 
 Send back the received data!
 
@@ -37,13 +29,14 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 5
-Byte: Data: 05 
+ID: 3
+Byte: Data: 03 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
+
 ID: 4
 Byte: Data: 04 
 
@@ -52,8 +45,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 7
-Byte: Data: 07 
+ID: 5
+Byte: Data: 05 
 
 Send back the received data!
 
@@ -68,8 +61,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 9
-Byte: Data: 09 
+ID: 7
+Byte: Data: 07 
 
 Send back the received data!
 
@@ -84,8 +77,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 11
-Byte: Data: 11 
+ID: 9
+Byte: Data: 09 
 
 Send back the received data!
 
@@ -100,8 +93,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 13
-Byte: Data: 13 
+ID: 11
+Byte: Data: 11 
 
 Send back the received data!
 
@@ -116,8 +109,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 15
-Byte: Data: 15 
+ID: 13
+Byte: Data: 13 
 
 Send back the received data!
 
@@ -132,8 +125,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 17
-Byte: Data: 17 
+ID: 15
+Byte: Data: 15 
 
 Send back the received data!
 
@@ -146,14 +139,10 @@ Byte: Data: 16
 Send back the received data!
 
 Message queued for transmission
-Alert: The Transmission was successful.
-
-TX buffered: 0	 
-
 Message is in Standard Format
 
-ID: 19
-Byte: Data: 19 
+ID: 17
+Byte: Data: 17 
 
 Send back the received data!
 
@@ -168,8 +157,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 21
-Byte: Data: 21 
+ID: 19
+Byte: Data: 19 
 
 Send back the received data!
 
@@ -184,8 +173,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 23
-Byte: Data: 23 
+ID: 21
+Byte: Data: 21 
 
 Send back the received data!
 
@@ -200,8 +189,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 25
-Byte: Data: 25 
+ID: 23
+Byte: Data: 23 
 
 Send back the received data!
 
@@ -216,8 +205,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 27
-Byte: Data: 27 
+ID: 25
+Byte: Data: 25 
 
 Send back the received data!
 
@@ -232,8 +221,8 @@ Send back the received data!
 Message queued for transmission
 Message is in Standard Format
 
-ID: 29
-Byte: Data: 29 
+ID: 27
+Byte: Data: 27 
 
 Send back the received data!
 
@@ -242,6 +231,14 @@ Message is in Standard Format
 
 ID: 28
 Byte: Data: 28 
+
+Send back the received data!
+
+Message queued for transmission
+Message is in Standard Format
+
+ID: 29
+Byte: Data: 29 
 
 Send back the received data!
 
@@ -257,7 +254,7 @@ Message queued for transmission
 Alert: The Transmission was successful.
 
 TX buffered: 0	 
- 
+
 */
 
 
@@ -278,7 +275,7 @@ void setup() {
   mcp2515.setNormalMode();
 
   delay(200);
-  for (uint8_t i = 1; i<=30; i+=2) {
+  for (uint8_t i = 0; i<=30; i+=2) {
   
   // num++;
   testData = i; // Generate random numbers (from 1 to 99) to simulate sensor data
@@ -287,7 +284,7 @@ void setup() {
   canMessage.can_dlc = 1; // Data Length Code
   canMessage.data[0] = testData;
   mcp2515.sendMessage(&canMessage); // Send CAN messaage
-  delay(25);
+  delay(12);
   }
   
 
