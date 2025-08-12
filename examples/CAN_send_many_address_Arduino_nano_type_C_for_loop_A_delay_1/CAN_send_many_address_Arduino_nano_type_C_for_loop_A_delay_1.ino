9d3e639a7ad50059d7b5ccca294de66751d9ffa6
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2025-08-12 10:49:20 +0700

    doc (exam): add test with delay 1 milisec and 8 bytes data

--- examples/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_1/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_1.ino
+++ examples/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_1/CAN_send_many_address_Arduino_nano_type_C_for_loop_A_delay_1.ino
@@ -2,27 +2,15 @@
 Message is in Standard Format
 
 ID: 1
-Byte: Data: 01 
+Byte: Data: 01 01 01 01 01 01 01 01 
 
 Send back the received data!
 
 Message queued for transmission
-Alert: The Transmission was successful.
-
-TX buffered: 0	 
-
 Message is in Standard Format
 
 ID: 3
-Byte: Data: 03 
-
-Send back the received data!
-
-Message queued for transmission
-Message is in Standard Format
-
-ID: 0
-Byte: Data: 00 
+Byte: Data: 03 03 03 03 03 03 03 03 
 
 Send back the received data!
 
@@ -30,15 +18,7 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 5
-Byte: Data: 05 
-
-Send back the received data!
-
-Message queued for transmission
-Message is in Standard Format
-
-ID: 2
-Byte: Data: 02 
+Byte: Data: 05 05 05 05 05 05 05 05 
 
 Send back the received data!
 
@@ -46,15 +26,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 7
-Byte: Data: 07 
+Byte: Data: 07 07 07 07 07 07 07 07 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 4
-Byte: Data: 04 
+ID: 0
+Byte: Data: 00 00 00 00 00 00 00 00 
 
 Send back the received data!
 
@@ -62,15 +42,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 9
-Byte: Data: 09 
+Byte: Data: 09 09 09 09 09 09 09 09 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 6
-Byte: Data: 06 
+ID: 2
+Byte: Data: 02 02 02 02 02 02 02 02 
 
 Send back the received data!
 
@@ -78,15 +58,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 11
-Byte: Data: 11 
+Byte: Data: 11 11 11 11 11 11 11 11 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 8
-Byte: Data: 08 
+ID: 4
+Byte: Data: 04 04 04 04 04 04 04 04 
 
 Send back the received data!
 
@@ -94,15 +74,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 13
-Byte: Data: 13 
+Byte: Data: 13 13 13 13 13 13 13 13 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 10
-Byte: Data: 10 
+ID: 6
+Byte: Data: 06 06 06 06 06 06 06 06 
 
 Send back the received data!
 
@@ -110,15 +90,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 15
-Byte: Data: 15 
+Byte: Data: 15 15 15 15 15 15 15 15 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 12
-Byte: Data: 12 
+ID: 8
+Byte: Data: 08 08 08 08 08 08 08 08 
 
 Send back the received data!
 
@@ -126,15 +106,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 17
-Byte: Data: 17 
+Byte: Data: 17 17 17 17 17 17 17 17 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 14
-Byte: Data: 14 
+ID: 10
+Byte: Data: 10 10 10 10 10 10 10 10 
 
 Send back the received data!
 
@@ -142,15 +122,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 19
-Byte: Data: 19 
+Byte: Data: 19 19 19 19 19 19 19 19 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 16
-Byte: Data: 16 
+ID: 12
+Byte: Data: 12 12 12 12 12 12 12 12 
 
 Send back the received data!
 
@@ -158,15 +138,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 21
-Byte: Data: 21 
+Byte: Data: 21 21 21 21 21 21 21 21 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 18
-Byte: Data: 18 
+ID: 14
+Byte: Data: 14 14 14 14 14 14 14 14 
 
 Send back the received data!
 
@@ -174,15 +154,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 23
-Byte: Data: 23 
+Byte: Data: 23 23 23 23 23 23 23 23 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 20
-Byte: Data: 20 
+ID: 16
+Byte: Data: 16 16 16 16 16 16 16 16 
 
 Send back the received data!
 
@@ -190,15 +170,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 25
-Byte: Data: 25 
+Byte: Data: 25 25 25 25 25 25 25 25 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 22
-Byte: Data: 22 
+ID: 18
+Byte: Data: 18 18 18 18 18 18 18 18 
 
 Send back the received data!
 
@@ -206,15 +186,15 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 27
-Byte: Data: 27 
+Byte: Data: 27 27 27 27 27 27 27 27 
 
 Send back the received data!
 
 Message queued for transmission
 Message is in Standard Format
 
-ID: 24
-Byte: Data: 24 
+ID: 20
+Byte: Data: 20 20 20 20 20 20 20 20 
 
 Send back the received data!
 
@@ -222,7 +202,23 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 29
-Byte: Data: 29 
+Byte: Data: 29 29 29 29 29 29 29 29 
+
+Send back the received data!
+
+Message queued for transmission
+Message is in Standard Format
+
+ID: 22
+Byte: Data: 22 22 22 22 22 22 22 22 
+
+Send back the received data!
+
+Message queued for transmission
+Message is in Standard Format
+
+ID: 24
+Byte: Data: 24 24 24 24 24 24 24 24 
 
 Send back the received data!
 
@@ -230,7 +226,7 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 26
-Byte: Data: 26 
+Byte: Data: 26 26 26 26 26 26 26 26 
 
 Send back the received data!
 
@@ -238,7 +234,7 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 28
-Byte: Data: 28 
+Byte: Data: 28 28 28 28 28 28 28 28 
 
 Send back the received data!
 
@@ -246,7 +242,7 @@ Message queued for transmission
 Message is in Standard Format
 
 ID: 30
-Byte: Data: 30 
+Byte: Data: 30 30 30 30 30 30 30 30 
 
 Send back the received data!
 
@@ -281,10 +277,17 @@ void setup() {
   testData = i; // Generate random numbers (from 1 to 99) to simulate sensor data
 
   canMessage.can_id  = i; // ID must be unique in a single CAN Bus network.
-  canMessage.can_dlc = 1; // Data Length Code
+  canMessage.can_dlc = 8; // Data Length Code
   canMessage.data[0] = testData;
+  canMessage.data[1] = testData;
+  canMessage.data[2] = testData;
+  canMessage.data[3] = testData;
+  canMessage.data[4] = testData;
+  canMessage.data[5] = testData;
+  canMessage.data[6] = testData;
+  canMessage.data[7] = testData;
   mcp2515.sendMessage(&canMessage); // Send CAN messaage
-  delay(3);
+  delay(1);
   }
   
 
